c8d93808a2671e36d12d5766a38e8bc8a93a7542
tobezero01 <ducnhuad@gmail.com>
2024-10-30 12:12:20 +0700

    setup project part 2

--- WeatherApiService/src/main/java/com/skyapi/weatherforecast/WeatherApiServiceApplication.java
+++ WeatherApiService/src/main/java/com/skyapi/weatherforecast/WeatherApiServiceApplication.java
@@ -1,13 +1,22 @@
 package com.skyapi.weatherforecast;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WeatherApiServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherApiServiceApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner  commandLineRunner() {
+		return runner -> {
+			System.out.println("Service started ...");
+		};
 	}
 
 }
